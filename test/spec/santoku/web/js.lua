f33dd109cca6b94936150042d2cda821f99602df
broma0 <121064585+broma0@users.noreply.github.com>
2023-06-24 15:55:33 +0000

    val.cpp: add mto_len
    src/santoku/web/val.cpp
      + mto_len
    
    also: Makefile, test/spec/santoku/web/js.lua

--- test/spec/santoku/web/js.lua
+++ test/spec/santoku/web/js.lua
@@ -1,5 +1,6 @@
 local assert = require("luassert")
 local test = require("santoku.test")
+local compat = require("santoku.compat")
 local val = require("santoku.web.val")
 
 
@@ -92,5 +93,11 @@ test("js", function ()
 
 
 
+
+  test("unpack a javascript array", function ()
+    local arr = val({ 1, 2, 3 }, true):lua()
+    local a, b, c = compat.unpack(arr)
+    assert.same({ 1, 2, 3 }, { a, b, c })
+  end)
 
 end)

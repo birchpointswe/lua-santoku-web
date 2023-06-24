e95abf57d0a47501da836f1c7f7ae88c9f733bfd
broma0 <121064585+broma0@users.noreply.github.com>
2023-06-24 00:59:32 +0000

    update client.lua server.lua

--- src/santoku/web/worker/rpc/server.lua
+++ src/santoku/web/worker/rpc/server.lua
@@ -1,6 +1,5 @@
 local js = require("santoku.web.js")
 local val = require("santoku.web.val")
-local tup = require("santoku.tuple")
 local compat = require("santoku.compat")
 
 local global = js.self
@@ -12,16 +11,7 @@ M.init = function (obj)
     local ch = ev.ports[0]
     local fn = ev.data[0]
     local args = ev.data:slice(1)
-    local out = val.array()
-    local ret = tup(obj[fn](compat.unpack(args)))
-
-
-
-    for i = 1, tup.len(ret()) do
-      out:set(i - 1, tup.get(i, ret()))
-    end
-
-
+    local out = val.array(obj[fn](compat.unpack(args)))
     ch:postMessage(out)
   end)
 end

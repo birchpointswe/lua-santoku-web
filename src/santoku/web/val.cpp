cb5deab21233e6b081bdd99f7d60623b7a8b38ab
broma0 <121064585+broma0@users.noreply.github.com>
2023-09-17 01:46:54 +0000

    update val.cpp

--- src/santoku/web/val.cpp
+++ src/santoku/web/val.cpp
@@ -910,14 +910,18 @@ int luaopen_santoku_web_val (lua_State *L) {
   lua_pop(L, 1);
 
   EM_ASM(({
-    Module.IDX_VAL_REF = new WeakMap();
-    Module.IDX_IS_LUA = new WeakSet();
+
+    Module.IDX_VAL_REF = new Map();
+    Module.IDX_IS_LUA = new Set();
+
+
   }));
 
   lua_newtable(L);
   lua_newtable(L);
-  lua_pushstring(L, "k");
-  lua_setfield(L, -2, "__mode");
+
+
+
   lua_setmetatable(L, -2);
   IDX_TBL_VAL = luaL_ref(L, LUA_REGISTRYINDEX);
 

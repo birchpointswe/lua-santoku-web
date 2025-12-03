62404fb2c947ff2d4c16884f1448a2b7fbc899c6
broma0 <121064585+broma0@users.noreply.github.com>
2025-12-03 12:44:22 -0500

    util.wasm.lua: add M.encode_url, M.parse_url
    lib/santoku/web/util.wasm.lua
      + M.encode_url, M.parse_url
    
    also: lib/santoku/web/pwa/sw.wasm.lua, make.lua

--- lib/santoku/web/pwa/sw.wasm.lua
+++ lib/santoku/web/pwa/sw.wasm.lua
@@ -280,20 +280,14 @@ return function (opts)
       return nil
     end
 
-
-    util.parse_path(url, tmp_parsed.path, tmp_parsed.params)
-    local pathname = "/" .. table.concat(tmp_parsed.path, "/")
-
+    util.parse_url(url, tmp_parsed)
+    local pathname = tmp_parsed.pathname or "/"
 
     local path = tbl.merge({}, tmp_parsed.path)
     local params = tbl.merge({}, tmp_parsed.params)
-
-
     if opts.routes[pathname] then
       return opts.routes[pathname], path, params
     end
-
-
     for pattern, handler in pairs(opts.routes) do
       local param_names = {}
       local regex = "^" .. pattern:gsub(":([^/]+)", function (name)
@@ -302,14 +296,12 @@ return function (opts)
       end) .. "$"
       local captures = { pathname:match(regex) }
       if #captures > 0 then
-
         for i, name in ipairs(param_names) do
           params[name] = captures[i]
         end
         return handler, path, params
       end
     end
-
     return nil
   end
 
